6e4c3c9d2d2063b801bcf58746c5b6c9f34108d5
Hiran Joy <hiranjoy7@gmail.com>
2025-07-31 23:15:27 +0530

    Connection error solved (changed db_shopping)

--- Assets/Connection/Connection.php
+++ Assets/Connection/Connection.php
@@ -2,7 +2,7 @@
 $Server = "localhost";
 $User = "root";
 $Password = "";
-$DB = "db_shopping";
+$DB = "db_wallet";
 $con = mysqli_connect($Server,$User,$Password,$DB);
 if(!$con){
 echo "connection failed";
