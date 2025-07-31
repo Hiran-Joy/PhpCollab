6edbe0ea4b4109f9238777720d6ca6fa50ae83cd
Hiran Joy <hiranjoy7@gmail.com>
2025-08-01 00:28:34 +0530

    Basic git commands

---
+++ Basic_Git_Commands.php
@@ -0,0 +1,7 @@
+<!-- 1.Forcing Git to recognize the case change-->
+
+<!-- git config core.ignorecase false -->
+
+<!-- git mv -f course.php Course.php
+git commit -m "Force rename: course.php to Course.php"
+git push  -->
