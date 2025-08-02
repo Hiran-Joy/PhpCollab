869a2b555e03df07a94dc0ab47d2a78abd8c25c2
Hiran Joy <hiranjoy7@gmail.com>
2025-08-02 20:49:21 +0530

    Course.php and Subject.php updated

--- Admin/Course.php
+++ Admin/Course.php
@@ -1,45 +1,146 @@
 <?php
+// ==============================================
+// DATABASE CONNECTION & INITIALIZATION
+// ==============================================
 include("../Assets/Connection/Connection.php");
+$course_name = "";
+$course_id = "";
+$hidden = "";
 
+// ==============================================
+// INSERT/UPDATE HANDLING
+// ==============================================
 if(isset($_POST['btn_submit']))
 {
-  $course=$_POST['txt_course'];
-  $insQuery="insert into tbl_course (course_name) values('".$course."')";
-  if($con->query($insQuery))
+  $course = $_POST['txt_course'];
+  $hidden = $_POST['txt_hidden'];
+  echo $hidden;
+  
+  if($hidden == "")
+  {
+    $insQuery = "insert into tbl_course (course_name) values('".$course."')";
+    if($con->query($insQuery))
+    {
+      ?>
+      <script>
+        alert("Course Name Inserted");
+        window.location="Course.php";
+      </script>
+      <?php
+    }
+  }
+  else
+  {
+    $upQuery = "update tbl_course set course_name='".$course."' where course_id='".$hidden."'";
+    if($con->query($upQuery))
+    {
+      ?>
+      <script>
+        alert("Course Name Updated");
+        window.location="Course.php";
+      </script>
+      <?php
+    }
+  }
+}
+?>
+
+<?php
+// ==============================================
+// DELETE HANDLING
+// ==============================================
+if(isset($_GET['did']))
+{
+  $delQuery = "delete from tbl_course where course_id='".$_GET['did']."'";
+  if($con->query($delQuery))
   {
     ?>
     <script>
-      alert("Course Name Inserted");
+      alert("Course Name Deleted");
       window.location="Course.php";
     </script>
     <?php
   }
 }
-
 ?>
 
-
+<?php
+// ==============================================
+// EDIT DATA FETCHING
+// ==============================================
+if(isset($_GET['eid']))
+{
+  $selQuery = "select * from tbl_course where course_id='".$_GET['eid']."'";
+  $row = $con->query($selQuery);
+  $data = $row->fetch_assoc();
+  $course_name = $data['course_name'];
+  $course_id = $data['course_id'];
+}
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Course</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Course</title>
 </head>
 
 <body>
-<form action="" method="post"><table width="294" border="1">
-  <tr>
-    <td width="114">Course Name</td>
-    <td width="164"><label for="txt_course"></label>
-      <input type="text" name="txt_course" id="txt_course" /></td>
-  </tr>
-  <tr>
-    <td colspan="2"><div align="center">
-      <input type="submit" name="btn_submit" id="btn_submit" value="Submit" />
-    </div></td>
-    </tr>
-</table>
-</form>
+  <!-- ============================================== -->
+  <!-- COURSE INPUT FORM -->
+  <!-- ============================================== -->
+  <form action="" method="post">
+    <table width="294" border="1">
+      <tr>
+        <td width="114">Course Name</td>
+        <td width="164">
+          <label for="txt_course"></label>
+          <input type="hidden" name="txt_hidden" id="txt_hidden" value="<?php echo $course_id ?>"/>
+          <input type="text" name="txt_course" id="txt_course" value="<?php echo $course_name ?>"/>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <div align="center">
+            <input type="submit" name="btn_submit" id="btn_submit" value="Submit" />
+          </div>
+        </td>
+      </tr>
+    </table>
+  </form>
+  
+  <br>
+  
+  <!-- ============================================== -->
+  <!-- COURSE LISTING TABLE -->
+  <!-- ============================================== -->
+  <form action="" method="post">
+    <table width="294" border="1">
+      <tr>
+        <td>SI No:</td>
+        <td>Course Name</td>
+        <td>Action</td>
+      </tr>
+      <?php
+      $selQuery = "select * from tbl_course";
+      $row = $con->query($selQuery);
+      $i = 0;
+      while($data = $row->fetch_assoc())
+      {
+        $i++
+        ?>
+        <tr>
+          <td><?php echo $i; ?></td>
+          <td><?php echo $data['course_name']; ?></td>
+          <td>
+            <a href="Course.php?did=<?php echo $data['course_id'] ?>">Delete</a>
+            <a href="Course.php?eid=<?php echo $data['course_id'] ?>">Edit</a>
+          </td>
+        </tr>
+        <?php
+      }
+      ?>
+    </table>
+  </form>
 </body>
 </html>
--- Admin/Subject.php
+++ Admin/Subject.php
@@ -1,62 +1,151 @@
 <?php
+// ==============================================
+// DATABASE CONNECTION & INITIALIZATION
+// ==============================================
 include("../Assets/Connection/Connection.php");
+$subject_name = "";
+$course_idd = "";
 
+// ==============================================
+// INSERT HANDLING
+// ==============================================
 if(isset($_POST['btn_submit']))
 {
-  $subject=$_POST['txt_subject'];
-  $course_id=$_POST['sel_course'];
-  $insQuery="insert into tbl_subject (course_id,subject_name) values('".$course_id."','".$subject."')";
+  $subject = $_POST['txt_subject'];
+  $course_id = $_POST['sel_course'];
+  $insQuery = "insert into tbl_subject (course_id,subject_name) values('".$course_id."','".$subject."')";
+  
   if($con->query($insQuery))
   {
     ?>
-        <script>
+    <script>
       alert("Subject Name Inserted");
       window.location="Subject.php";
     </script>
     <?php
   }
-
 }
 ?>
 
+<?php
+// ==============================================
+// DELETE HANDLING
+// ==============================================
+if(isset($_GET['did']))
+{
+  $delQuery = "delete from tbl_subject where subject_id='".$_GET['did']."'";
+  if($con->query($delQuery))
+  {
+    ?>
+    <script>
+      alert("Subject Name Deleted");
+      window.location="Subject.php";
+    </script>
+    <?php
+  }
+}
+?>
+
+<?php
+// ==============================================
+// EDIT DATA FETCHING
+// ==============================================
+if(isset($_GET['eid']))
+{
+  $selQuery = "select * from tbl_subject where subject_id='".$_GET['eid']."'";
+  $row = $con->query($selQuery);
+  $data = $row->fetch_assoc();
+  $subject_name = $data['subject_name'];
+  $course_idd = $data['course_id'];
+}
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Subject Management</title>
 </head>
 
-<body><form action="" method="post"><table width="261" border="1">
-  <tr>
-    <td width="94">Course Name</td>
-    <td width="151"><label for="sel_course"></label>
-      <select name="sel_course" id="sel_course">
-      <option>...Select...</option>
-      <?php
-       $selQuery="select * from tbl_course";
-       $row=$con->query($selQuery);
-       while($data=$row->fetch_assoc())
-       {
-        ?>
-        <option value="<?php echo $data['course_id'] ?>"><?php echo $data['course_name'] ?></option>
-        <?php
-       }
-      ?>
+<body>
+  <!-- ============================================== -->
+  <!-- SUBJECT INPUT FORM -->
+  <!-- ============================================== -->
+  <form action="" method="post">
+    <table width="261" border="1">
+      <tr>
+        <td width="94">Course Name</td>
+        <td width="151">
+          <label for="sel_course"></label>
+          <select name="sel_course" id="sel_course">
+            <option>...Select...</option>
+            <?php
+            $selQuery = "select * from tbl_course";
+            $row = $con->query($selQuery);
+            while($data = $row->fetch_assoc())
+            {
+              ?>
+              <option 
+                <?php if($course_idd == $data['course_id']) echo "selected"; ?>
+                value="<?php echo $data['course_id']; ?>">
+                <?php echo $data['course_name']; ?>
+              </option>
+              <?php
+            }
+            ?>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <td>Subject Name</td>
+        <td>
+          <label for="txt_subject"></label>
+          <input type="text" name="txt_subject" id="txt_subject" value="<?php echo $subject_name ?>" />
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <div align="center">
+            <input type="submit" name="btn_submit" id="btn_submit" value="Submit" />
+          </div>
+        </td>
+      </tr>
+    </table>
+  </form>
 
-      </select></td>
-  </tr>
-  <tr>
-    <td>Subject Name</td>
-    <td><label for="txt_subject"></label>
-      <input type="text" name="txt_subject" id="txt_subject" /></td>
-  </tr>
-  <tr>
-    <td colspan="2"><div align="center">
-      <input type="submit" name="btn_submit" id="btn_submit" value="Submit" />
-    </div></td>
-    </tr>
-</table>
-</form>
+  <!-- ============================================== -->
+  <!-- SUBJECT LISTING TABLE -->
+  <!-- ============================================== -->
+  <form action="" method="post">
+    <table width="294" border="1">
+      <tr>
+        <td>SI No:</td>
+        <td>Course Name</td>
+        <td>Subject Name</td>
+        <td>Action</td>
+      </tr>
+      <br>
+      <?php
+      $selQuery = "select * from tbl_subject s inner join tbl_course c on s.course_id=c.course_id";
+      $row = $con->query($selQuery);
+      $i = 0;
+      while($data = $row->fetch_assoc())
+      {
+        $i++
+        ?>
+        <tr>
+          <td><?php echo $i; ?></td>
+          <td><?php echo $data['course_name']; ?></td>
+          <td><?php echo $data['subject_name']; ?></td>
+          <td>
+            <a href="Subject.php?did=<?php echo $data['subject_id'] ?>">Delete</a>
+            <a href="Subject.php?eid=<?php echo $data['subject_id'] ?>">Edit</a>
+          </td>
+        </tr>
+        <?php
+      }
+      ?>
+    </table>
+  </form>
 </body>
 </html>
