36f157bb4722b7ee418e9e13594ee504892a2555
Hiran Joy <hiranjoy7@gmail.com>
2025-08-03 20:10:09 +0530

    Noteupload.php almost complete(edit is pending)

---
+++ Assets/AjaxPages/AjaxSubject.php
@@ -0,0 +1,12 @@
+<?php
+include("../Connection/Connection.php");
+
+$selQuery="select * from tbl_subject where course_id='".$_GET['Did']."'";
+$row=$con->query($selQuery);
+	while($data=$row->fetch_assoc())
+	{
+		?>
+		<option value="<?php echo  $data['subject_id']?>"><?php echo  $data['subject_name']?></option>
+		<?php
+	}
+?>
