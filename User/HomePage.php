90715509a1d43f329f7445129d2366e1eaa167d9
Jais Jose <jaisjose44@gmail.com>
2025-08-08 22:41:58 +0530

    Feedback completed

--- User/HomePage.php
+++ User/HomePage.php
@@ -13,5 +13,6 @@ include("../Assets/Connection/Connection.php");
     <h1>Welcome <?php echo $_SESSION['uname'];  ?></h1>
     <a href="MyProfile.php">MyProfile</a>
     <a href="Noteupload.php">NoteUpload</a>
+     <a href="FeedBack.php">Feedback</a>
 </body>
 </html> 
