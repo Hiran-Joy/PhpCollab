0412c4a1ba83625974acd82d9fec25ecce2b8a32
Hiran Joy <hiranjoy7@gmail.com>
2025-08-10 22:06:54 +0530

    Noteupload Completed

--- Assets/AjaxPages/AjaxSubject.php
+++ Assets/AjaxPages/AjaxSubject.php
@@ -1,12 +1,18 @@
 <?php
 include("../Connection/Connection.php");
 
-$selQuery="select * from tbl_subject where course_id='".$_GET['Did']."'";
-$row=$con->query($selQuery);
-	while($data=$row->fetch_assoc())
-	{
-		?>
-		<option value="<?php echo  $data['subject_id']?>"><?php echo  $data['subject_name']?></option>
-		<?php
-	}
+$subject_idd = isset($_GET['Sid']) ? $_GET['Sid'] : "";
+
+$selQuery = "SELECT * FROM tbl_subject WHERE course_id='".$_GET['Did']."'";
+$row = $con->query($selQuery);
+
+while($data = $row->fetch_assoc())
+{
+    ?>
+    <option value="<?php echo $data['subject_id']; ?>"
+        <?php if($subject_idd == $data['subject_id']) { echo "selected"; } ?>>
+        <?php echo $data['subject_name']; ?>
+    </option>
+    <?php
+}
 ?>
